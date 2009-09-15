1f03045eaecff067638cfdb8668fb290472521c0
Morgan 'ARR!' Allen <morganrallen@gmail.com>
2009-09-15 07:04:51 +0200

    forgot to git add CatchAll/jscatchall.js

--- CatchAll/jscatchall.js
+++ CatchAll/jscatchall.js
@@ -1,11 +1,33 @@
-// usage
-// var class = new CatchAll(baseClass, function(variable, value) {
-//  doSomething(variable, value);
-// }
+/*
+usage
+
+function baseClass()
+{
+};
+
+baseClass.prototype = { blah: 1 };
+
+var class = new CatchAll(baseClass, function(variable, value) {
+    // do not allow blah to be changed
+    if(variable == 'blah')
+        return false;
+});
+
+var classInstance = new class();
+classInstance.blah = 2; // still revert back to 1
+
+Downfalls:
+The obvious problem here is the race condition. 
+if you try something like (with the length_enforce example)
+
+morgan.name = 'greaterthen10';morgan.getName();
+
+it will return 'greaterthen10', then it will revert
+*/
 var CatchAll = (function()
 {
     var timer = false,
-        timeout = 60,
+        timeout = 13,
         observedObjects = [];
 
     function runPropCheck()
@@ -30,7 +52,7 @@ var CatchAll = (function()
     ObservableObject.prototype.check = function()
     {
         for(var i in this._obj) {
-            if(this._props[i] != this._obj[i] || !this._props[i])
+            if(this._props[i] != this._obj[i] || (this._obj[i] && !this._props[i]))
             {
                 if(this._fnCatchAll.call(this._scope || this, i, this._obj[i]) !== false) {
                     this._props[i] = this._obj[i];
